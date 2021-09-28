6ca36e5a682644e3e9dc87ebac9d39d1dd760d62
Gon <29622355+gonyyi@users.noreply.github.com>
2021-09-28 12:06:34 -0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,8 @@ func main() {
 	ignore := []string{
 		"/favicon.ico",
 	}
-	err := reqtest.Run(":8080", "test", 20, ignore, nil)
+
+	err := reqtest.Run(":10173", "test123","r", 20, ignore, nil)
 	if err != nil {
 		println(err.Error())
 	}
