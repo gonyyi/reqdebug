3c36440ba032d3204e6b6ac15cd677bc4b4f1198
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-01 14:24:29 -0600

    Update reqtest.go

--- reqtest.go
+++ reqtest.go
@@ -1,8 +1,12 @@
+// (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
+// Last Update: 12/01/2021
+
 package reqtest
 
 import (
 	"embed"
-	"github.com/gonyyi/common"
+	"fmt"
+	"github.com/gonyyi/gosl"
 	"html/template"
 	"net/http"
 	"net/http/httputil"
@@ -11,28 +15,14 @@ import (
 	"time"
 )
 
-const VERSION = "ReqTest v1.1.0"
+const Version gosl.Ver = "ReqTest v1.2.0"
 
 var (
-	respTmpl      *template.Template
-	lastData      []Data
-	lastDataIndex common.RollingIndex
-	serviceName   string
 	//go:embed template.html
 	tmpl embed.FS
 )
 
-type Err string
-
-func (e Err) Error() string {
-	return string(e)
-}
-
-const (
-	ERR_BAD_ADDRESS = Err("unexpected address is given (expected: ':8080')")
-)
-
-type Data struct {
+type data struct {
 	ServiceName    string
 	ServiceVersion string
 	Mode           string
@@ -47,151 +37,165 @@ type Data struct {
 	DebugURL       string
 	ReqNo          int
 	ReqList        []string
-	Param          string
 }
 
-func debugHandler(idx int, param string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		var reqList []string
-		currListSize := len(lastDataIndex.List()) // current list size.. it can be 0 to reqsKeep
-		for i := 0; i < currListSize; i++ {
-			reqList = append(reqList, strconv.Itoa(i))
-		}
+// New creates a request tester
+// This panics when parsing handler is failed
+func New(name string, debugURI string, reqsKeep int, ignoreURIs []string) *reqTest {
+	rt := &reqTest{}
+	rt.serviceName = name
+	rt.lastData = make([]data, reqsKeep)
+	rt.lastDataIndex = newRollingIndex(reqsKeep)
+	rt.debugURI = debugURI
 
-		// fmt.Printf("ReqList: %v\n", reqList)
-		if idx+1 > len(lastData) {
-			respTmpl.Execute(w, Data{
-				Mode:           "::debug",
-				ServiceName:    serviceName,
-				ServiceVersion: VERSION,
-				Error:          "Index outside the range",
-				ReqList:        reqList,
-				ReqNo:          idx,
-				Param:          param,
-			})
-			return
-		}
-		if idx+1 > currListSize || currListSize == 0 {
-			respTmpl.Execute(w, Data{
-				Mode:           "::debug",
-				ServiceName:    serviceName,
-				ServiceVersion: VERSION,
-				Error:          "No data",
-				ReqList:        reqList,
-				ReqNo:          idx,
-				Param:          param,
-			})
-			return
-		}
-
-		{
-			tmpData := lastData[lastDataIndex.Curr()-idx]
-			tmpData.Request = strings.TrimSpace(tmpData.Request)
-			tmpData.Mode = "::debug"
-			tmpData.DebugURL = ""
-			tmpData.ReqList = reqList
-			tmpData.ReqNo = idx
-			tmpData.Param = param
-			respTmpl.Execute(w, tmpData)
-		}
+	// IGNORE SOME URIs
+	rt.ignores = make(map[string]struct{})
+	for _, v := range ignoreURIs {
+		rt.ignores[v] = struct{}{}
 	}
+	var err error
+	if rt.respTmpl, err = template.ParseFS(tmpl, "template.html"); err != nil {
+		panic(err)
+		return nil
+	}
+	return rt
 }
 
-func newHandler(name string, param string) http.HandlerFunc {
+type reqTest struct {
+	serviceName   string
+	lastData      []data
+	lastDataIndex rollingIndex
+	ignores       map[string]struct{}
+	debugURI      string
+	respTmpl      *template.Template
+}
+
+// DefaultHandler shows a page where request can be view as a response to the request
+func (rt *reqTest) DefaultHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		curr := 0
+		if _, ok := rt.ignores[r.URL.RequestURI()]; !ok { // don't do anything for ignore URI lists
+			curr = rt.tracer("Default Handler", r)
+		}
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		lastDataIndex = lastDataIndex.Next()
-		lastData[lastDataIndex.Curr()] = Data{
-			ServiceName:    serviceName,
-			ServiceVersion: VERSION,
-			Mode:           "",
-			Time:           time.Now().Format("2006/01/02 15:04:05.000"),
-			Name:           name,
-			Host:           r.Host,
-			Path:           r.URL.Path,
-			URI:            r.RequestURI,
-			IP:             r.RemoteAddr,
-			DebugURL:       "",
-		}
-
-		{
-			scheme := "https"
-			if r.TLS == nil {
-				scheme = "http"
-			}
-			lastData[lastDataIndex.Curr()].DebugURL = scheme + "://" + r.Host + r.URL.Path + "?" + param + "=0"
-		}
-
-		reqOut, err := httputil.DumpRequest(r, true)
-		if err != nil {
-			lastData[lastDataIndex.Curr()].Error = err.Error()
-		} else {
-			lastData[lastDataIndex.Curr()].Error = ""
-		}
-		lastData[lastDataIndex.Curr()].Request = strings.TrimSpace(string(reqOut))
-		if err := respTmpl.Execute(w, lastData[lastDataIndex.Curr()]); err != nil {
+		if err := rt.respTmpl.Execute(w, rt.lastData[curr]); err != nil {
 			println(err.Error())
 		}
 	}
 }
 
-func Run(addr string, name string, param string, reqsKeep int, ignoreURIs []string, customHandler map[string]http.HandlerFunc) (err error) {
-	param = strings.TrimSpace(param)
-	if param == "" {
-		param = "q"
-	}
+// debugHandler traces
+func (rt *reqTest) debugHandler(idx int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		var reqList []string
+		currListSize := len(rt.lastDataIndex.List()) // current list size.. it can be 0 to reqsKeep
+		for i := 0; i < currListSize; i++ {
+			reqList = append(reqList, strconv.Itoa(i))
+		}
 
-	serviceName = name
-	lastData = make([]Data, reqsKeep)
-	lastDataIndex = common.NewRollingIndex(reqsKeep)
+		// fmt.Printf("ReqList: %v\n", reqList)
+		if idx+1 > len(rt.lastData) {
+			rt.respTmpl.Execute(w, data{
+				Mode:           "::debug",
+				ServiceName:    rt.serviceName,
+				ServiceVersion: Version.String(),
+				Error:          "Index outside the range",
+				ReqList:        reqList,
+				ReqNo:          idx,
+				DebugURL:       rt.debugURI,
+			})
+			return
+		}
+		if idx+1 > currListSize || currListSize == 0 {
+			rt.respTmpl.Execute(w, data{
+				Mode:           "::debug",
+				ServiceName:    rt.serviceName,
+				ServiceVersion: Version.String(),
+				Error:          "No data",
+				ReqList:        reqList,
+				ReqNo:          idx,
+				DebugURL:       rt.debugURI,
+			})
+			return
+		}
 
-	// IGNORE SOME URIs
-	ignores := make(map[string]struct{})
-	for _, v := range ignoreURIs {
-		ignores[v] = struct{}{}
-	}
-
-	// LOAD TEMPLATE
-	respTmpl, err = template.ParseFS(tmpl, "template.html")
-	if err != nil {
-		return err
-	}
-	if !strings.Contains(addr, ":") {
-		return ERR_BAD_ADDRESS
-	}
-
-	// HANDLERS
-	if customHandler == nil {
-		customHandler = make(map[string]http.HandlerFunc)
-	}
-	defaultHandler := newHandler("Default", param)
-
-	router := func(w http.ResponseWriter, r *http.Request) {
-		// if URL has a query key "reqtest" with a value, then it's a debug mode.
-		// if request URI is in ignoreURLs, do not respond
-		if idx, ok := r.URL.Query()[param]; ok {
-			intIdx := 0
-			if len(idx) > 0 {
-				intIdx, _ = strconv.Atoi(idx[0])
-			}
-			debugHandler(intIdx, param)(w, r)
-		} else if _, ok := ignores[r.URL.RequestURI()]; !ok { // don't do anything for ignore URI lists
-			// see if customHandler is exist
-			url := strings.SplitN(r.Host, ":", 2)[0]
-			if h, ok := customHandler[url]; ok && h != nil {
-				h(w, r)
-			} else {
-				defaultHandler(w, r)
-			}
+		{
+			tmpData := rt.lastData[rt.lastDataIndex.Curr()-idx]
+			tmpData.Request = strings.TrimSpace(tmpData.Request)
+			tmpData.Mode = "::debug"
+			tmpData.DebugURL = rt.debugURI
+			tmpData.ReqList = reqList
+			tmpData.ReqNo = idx
+			rt.respTmpl.Execute(w, tmpData)
 		}
 	}
+}
 
-	http.HandleFunc("/", router)
-	println(VERSION + " <https://gonyyi.com/copyright>")
-	println("Starting at <" + addr + ">")
-	return http.ListenAndServe(addr, nil)
+// DebugHandler is to be used for debug page where requests can be viewed
+func (rt *reqTest) DebugHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var idx int
+		// Get debug number..
+		if r.URL != nil && strings.HasPrefix(r.URL.Path, rt.debugURI) {
+			println(1, r.URL.Path)
+			tmp := strings.Split(strings.TrimPrefix(r.URL.Path, rt.debugURI), "/") // /debug/123/ --> ["", "123", ""] or ["123", ""] depend on debugURI is `/debug` or `/debug/`
+			for _, v := range tmp {
+				if v != "" {
+					idx = gosl.MustAtoi(v, 0)
+					break
+				}
+			}
+		}
+		rt.debugHandler(idx)(w, r)
+	}
+}
+
+// tracer records the trace
+func (rt *reqTest) tracer(name string, r *http.Request) (curr int) {
+	rt.lastDataIndex = rt.lastDataIndex.Next()
+	curr = rt.lastDataIndex.Curr()
+	rt.lastData[curr] = data{
+		ServiceName:    rt.serviceName,
+		ServiceVersion: Version.String(),
+		Mode:           "",
+		Time:           time.Now().Format("2006/01/02 15:04:05.000"),
+		Name:           name,
+		Host:           r.Host,
+		Path:           r.URL.Path,
+		URI:            r.RequestURI,
+		IP:             r.RemoteAddr,
+		DebugURL:       "",
+	}
+	{
+		var scheme string
+		if r.TLS == nil {
+			scheme = "http"
+		} else {
+			scheme = "https"
+		}
+		rt.lastData[curr].DebugURL = fmt.Sprintf("%s://%s/%s/%d", scheme, r.Host, rt.debugURI, 0)
+		// rt.lastData[curr].DebugURL = scheme + "://" + r.Host + "/" + rt.debugURI + "/"
+		// rt.lastData[curr].DebugURL = scheme + "://" + r.Host + r.URL.Path + "?" + param + "=0"
+	}
+	reqOut, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		rt.lastData[curr].Error = err.Error()
+	} else {
+		rt.lastData[curr].Error = ""
+	}
+	rt.lastData[curr].Request = strings.TrimSpace(string(reqOut))
+	return curr
+}
+
+// TraceWrapper wraps handler func with a tracer
+func (rt *reqTest) TraceWrapper(hf http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if _, ok := rt.ignores[r.URL.RequestURI()]; !ok { // don't do anything for ignore URI lists
+			rt.tracer("Custom Handler", r)
+			hf(w, r)
+		}
+	}
 }
