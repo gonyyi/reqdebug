17e479512ca60e5b23c47f40837791d081cc0854
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-01 14:24:08 -0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,22 @@
+// (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
+// Last Update: 12/01/2021
+
 package main
 
 import (
 	"github.com/gonyyi/reqtest"
+	"net/http"
 )
 
 func main() {
-	ignore := []string{
-		"/favicon.ico",
-	}
+	reqtest.SimpleRun(":8080")
+}
 
-	err := reqtest.Run(":10173", "test123","r", 20, ignore, nil)
-	if err != nil {
-		println(err.Error())
-	}
+func main1() {
+	rt := reqtest.New("t1", "/debug", 10, []string{"/favicon.ico"}) // New creates a testing service
+	hello := func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello world test")) }
+	http.HandleFunc("/test/", rt.TraceWrapper(hello)) // TraceWrapper takes a handler function and run trace
+	http.HandleFunc("/debug/", rt.DebugHandler())     // DebugHandler shows all requests
+	http.HandleFunc("/", rt.DefaultHandler())         // DefaultHandler returns debug info of the request -eg. for web browser
+	http.ListenAndServe(":8080", nil)
 }
