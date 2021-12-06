2a4b076d1863d5e986a05e111b7f98e4627bea69
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-06 14:46:25 -0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,32 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 12/01/2021
+// Last Update: 12/06/2021
 
 package main
 
 import (
+	"github.com/gonyyi/gosl"
 	"github.com/gonyyi/reqtest"
-	"net/http"
+	"os"
 )
 
 func main() {
-	reqtest.SimpleRun(":8080")
+	println(reqtest.Version + " " + "<https://github.com/gonyyi/reqtest>")
+
+	if len(os.Args) < 2 {
+		println("Usage: "+os.Args[0]+" <PORT>")
+		return
+	}
+	// if not number, then report an error
+	if !gosl.IsNumber(os.Args[1]) {
+		println("Error: <PORT> must be an integer (1-65535)")
+		return
+	}
+	if err := reqtest.SimpleRun(":" + os.Args[1]); err != nil {
+		println(err.Error())
+	}
 }
 
-func main1() {
+func Example() {
 	rt := reqtest.New("t1", "/debug", 10, []string{"/favicon.ico"}) // New creates a testing service
 	hello := func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello world test")) }
 	http.HandleFunc("/test/", rt.TraceWrapper(hello)) // TraceWrapper takes a handler function and run trace
