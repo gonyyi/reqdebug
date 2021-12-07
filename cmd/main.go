e2220b681a3e42ca661cb466ea650c5238d0bd7f
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-07 14:55:47 -0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,33 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 12/06/2021
+// Last Update: 12/07/2021
 
 package main
 
 import (
-	"github.com/gonyyi/gosl"
 	"github.com/gonyyi/reqtest"
-	"os"
+	"net/http"
 )
 
 func main() {
-	println(reqtest.Version + " " + "<https://github.com/gonyyi/reqtest>")
+	normal()
+}
 
-	if len(os.Args) < 2 {
-		println("Usage: "+os.Args[0]+" <PORT>")
-		return
-	}
-	// if not number, then report an error
-	if !gosl.IsNumber(os.Args[1]) {
-		println("Error: <PORT> must be an integer (1-65535)")
-		return
-	}
-	if err := reqtest.SimpleRun(":" + os.Args[1]); err != nil {
+func simple() {
+	println("Starting " + reqtest.Version)
+	if err := reqtest.SimpleRun(":8080", ":8089"); err != nil {
 		println(err.Error())
 	}
 }
 
-func Example() {
-	rt := reqtest.New("t1", "/debug", 10, []string{"/favicon.ico"}) // New creates a testing service
+func normal() {
+	rt := reqtest.New("t1", ":8089", 10, "/favicon.ico")
 	hello := func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello world test")) }
+
+	println("ServiceAddr: <" + ":8080" + ">")
+	println("ViewerAddr:  <" + rt.ViewerURL() + ">")
+
 	http.HandleFunc("/test/", rt.TraceWrapper(hello)) // TraceWrapper takes a handler function and run trace
-	http.HandleFunc("/debug/", rt.DebugHandler())     // DebugHandler shows all requests
+	http.HandleFunc("/debug/", rt.ViewHandler())      // ViewHandler shows all requests
 	http.HandleFunc("/", rt.DefaultHandler())         // DefaultHandler returns debug info of the request -eg. for web browser
 	http.ListenAndServe(":8080", nil)
 }
