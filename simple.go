d4eb5115c33f69c32f2f93d8ba22ee8c373e2c89
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-07 14:56:29 -0600

    Update simple.go

--- simple.go
+++ simple.go
@@ -1,5 +1,5 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 12/01/2021
+// Last Update: 12/07/2021
 
 package reqtest
 
@@ -8,9 +8,11 @@ import "net/http"
 // SimpleRun starts only with the address like `:8080`.
 // It's debug URI is `/debug` also keeps up to 20 records,
 // and ignores request for `favicon.ico`
-func SimpleRun(addr string) error {
-	rt := New("SimpleRun", "/debug", 20, []string{"/favicon.ico"}) // New creates a testing service
-	http.HandleFunc("/debug/", rt.DebugHandler())           // DebugHandler shows all requests
-	http.HandleFunc("/", rt.DefaultHandler())               // DefaultHandler returns debug info of the request -eg. for web browser
-	return http.ListenAndServe(addr, nil)
+func SimpleRun(serviceAddr, viewerAddr string) error {
+	// New creates a testing service
+	rt := New("ReqTest", viewerAddr, 20, "/favicon.ico")
+	http.HandleFunc("/", rt.DefaultHandler()) // DefaultHandler returns debug info of the request -eg. for web browser
+	println("ServiceAddr: <" + serviceAddr + ">")
+	println("ViewerAddr:  <" + rt.ViewerURL() + ">")
+	return http.ListenAndServe(serviceAddr, nil)
 }
